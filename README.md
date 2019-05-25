70a0dfc354404e0dd8368f2128cd9f2f460ca445
StarkOne <Stark.pro40@gmail.com>
2019-05-25 17:36:38 +0700

    мелкие правки

--- README.md
+++ README.md
@@ -11,7 +11,7 @@
 Инструкция где взять свой  token
 -------------
 ![](https://downloader.disk.yandex.ru/preview/63551c3b0a63f5cb97eb64c408f6d82e4f5107f24171ebaeef7f36d39e6f776e/5ce95045/YeqlB16h-j60z0QnLrouSCesnTDucVKLDYZUFDKEnszYQSgEqlkyxesLB5GgymmMu7Ca6eJjPJOEF7Kws7YCHg%3D%3D?uid=0&filename=2019-05-25_17-23-46.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&tknv=v2&size=2048x2048)
-## 
+##
 ![](https://4.downloader.disk.yandex.ru/preview/f75e262a548dbac97bf8be4ba7fb67171a8ff36b38e3784221ae7e92d96d1b5b/inf/Znah6BiZrzJfvHcvoqBdlhdLPK6nv0SiK9Yo_rqvBnYYOZAR3nB1yblXg3rKtWYwgmTPM7tI1Rnm00h7gRIw1Q%3D%3D?uid=98238635&filename=2019-05-25_17-26-07.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&tknv=v2&size=1903x882)
-## 
+##
 ![](https://downloader.disk.yandex.ru/preview/42534f1f2e9f924b4d2a71d1afc780372aae985f4f24df29acf95b994bb9b362/5ce950db/wpZq_hyiN_SZkei7OFQDUVL26np6txwSNOOXJ464nX8IzLVLGXIw_Vu1P-z7Be3hyOK7ZeOwwaG7Z165Bf2bIQ%3D%3D?uid=0&filename=2019-05-25_17-27-30.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&tknv=v2&size=2048x2048)
--- index.js
+++ index.js
@@ -1,21 +1,25 @@
-const { token, url, outstandingTasks, me } = require("./config");
+const { token, url, outstandingTasks, me, costPerHour } = require("./config");
 const axios = require("axios");
 const Spinner = require("cli-spinner").Spinner;
 const sp = new Spinner();
 sp.setSpinnerString(18);
 const report = require("./report");
-try {
-  axios.defaults.headers["Authorization"] = token;
-  axios.defaults.baseURL = "https://jilfond.myjetbrains.com";
-} catch (e) {}
+if (token && costPerHour) {
+  try {
+    axios.defaults.headers["Authorization"] = token;
+    axios.defaults.baseURL = "https://jilfond.myjetbrains.com";
+  } catch (e) { }
 
-(async () => {
-  sp.start();
-  const [monthTask, unfulfilled, user] = await Promise.all([
-    axios.get(url),
-    axios.get(outstandingTasks),
-    axios.get(me)
-  ]);
-  await report.createReport(monthTask, unfulfilled, user);
-  sp.stop();
-})();
+  (async () => {
+    sp.start();
+    const [monthTask, unfulfilled, user] = await Promise.all([
+      axios.get(url),
+      axios.get(outstandingTasks),
+      axios.get(me)
+    ]);
+    await report.createReport(monthTask, unfulfilled, user);
+    sp.stop();
+  })();
+} else {
+  console.log('Вы забыли написать свой token или costPerHour в config.js'.bgRed);
+}
