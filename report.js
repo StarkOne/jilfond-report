cc7fcc8490a54741de3711d4e4b956047b07265a
StarkOne <Stark.pro40@gmail.com>
2019-05-25 11:37:13 +0700

    filters task done

--- report.js
+++ report.js
@@ -1,8 +1,16 @@
 const fs = require('fs');
 const path = require('path');
-
+let moment = require('moment');
+moment.locale('ru');
+let date = moment().format('MMMM - GGGG');
+let statusDone = [];
+const filterStatus = data => {
+  statusDone = data.filter(item => item.status == "Внедрено")
+}
 const createReport = (data) => {
-  let text = "";
+  let text = `${date}\n`;
+  filterStatus(data);
+  console.log(statusDone);
   data.map(item => {
     text += `${item.title};затрачено - ${item.spent}м; состояние - ${item.status};\n`
   });
